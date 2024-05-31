89a4a6d1c4d4804bdce323cd7a597668b0c0e2b2
elenitacaca <elenita23235@gmail.com>
2024-05-31 21:29:28 +0200

    menumuerteINTENTOFALLIDO

--- Assets/Scripts/Menumuerte.cs
+++ Assets/Scripts/Menumuerte.cs
@@ -1,34 +1,79 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class Menumuerte : MonoBehaviour
+
 {
-    public float vida;
-    GameObject panel;
-    // Start is called before the first frame update
+ GameObject rip;
+    GameObject retry;
+    GameObject home;
+    private int vida;
+    private int estoyMuerto;
     void Start()
-    {
-        panel.SetActive(false);
-         if(vida == 0){
-         panel = GameObject.Find("Panel");
-        panel.SetActive(true);
+{
+       rip = GameObject.Find("rip");
+       rip.SetActive(false);
+
+     if(GameManager.estoyMuerto){
+         rip = GameObject.Find("rip");
+           rip.SetActive(true);
+
         }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        retry = GameObject.Find("retry");
+        retry.SetActive(false);
         
-    }
 
-    public void StartGame(){
+         if(GameManager.estoyMuerto){
+         retry = GameObject.Find("retry");
+           retry.SetActive(true);
+
+        }
+
+        home = GameObject.Find("home");
+        home.SetActive(false);
+        
+
+         if(GameManager.estoyMuerto){
+         home = GameObject.Find("home");
+           home.SetActive(true);
+         }
+}
+public void reiniciar()
+{
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+}
+
+ public void Menuprincipal(String nombre)
+ {
+     SceneManager.LoadScene(nombre);
+ }
+
+ public void StartGame(){
 
         SceneManager.LoadScene("Escena1");
         }
 
-        public void Menuprincipal(){
+      public void Menuprincipal(){
        
         SceneManager.LoadScene("Menuprincipal");
-    }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+   
