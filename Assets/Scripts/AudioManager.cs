d2bff29496b7598767df9a9e039d9fba69e09691
frogsand <lasranasdesatan@gmail.com>
2024-06-03 12:27:15 +0200

    Cambio Script

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,33 +4,6 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-
-    public AudioClip bandaSonora;
-
-    public AudioClip fxButton;
-
-    public AudioClip fxCoin;
-
-    public AudioClip fxDead;
-
-    public AudioClip fxFire;
-
-    AudioSource _audioSource;
-
-    public static AudioManager Instance;
-
-    void Awake(){
-
-        if(instance != null && Instance!= this){
-            Destroy(this.gameObject);
-        }else{
-            Instance = this;
-            DontDestroyOnLoad(this.gameobject);
-        }
-        
-    }
-    
-
     // Start is called before the first frame update
     void Start()
     {
